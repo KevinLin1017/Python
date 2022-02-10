79aa8682d3dd3c1d2d134c499a41a2e2c666b5ce
Kevin <kevin.lin@smoothstack.com>
2022-02-09 21:44:07 -0800

    v2

--- 02082022.py
+++ 02082022.py
@@ -28,7 +28,7 @@ isPalindrome(a)
 isPalindrome(b)
 isPalindrome(c)
 
-
+# -----------------------------------------------------------------------------
 print("\n \n  Coding Exercise 4: \n")
 
 a = [1, "hello world!", 2.5]
@@ -40,7 +40,44 @@ print("3. ", lst[1:])
 weekdays = {"monday": 1, "tuesday": 2, "wednesday": 3, "thursday": 4, "friday": 5}
 print("4. {}".format(weekdays))
 D = {"k1": [1, 2, 3]}
+# print(D[k1][1]) Need to provide index not the value
+mytuple = ([1, [2, 3]], 0)
+print("5. {}".format(mytuple))
 
+
+def distinctCharacter(s):
+    n = "".join(set(s))
+    return n
+
+
+s = "Mississippi"
+ans = distinctCharacter(s)
+print("6. {}".format(ans))
+ans += "X"
+print("7. {}".format(ans))
+
+mylist = []
+x = range(2002, 3000, 7)
+for n in x:
+    if (n % 5) != 0:
+        mylist.append(n)
+    else:
+        pass
+
+print("8. {}".format(mylist))
+
+
+def fact(x):
+    if x == 0:
+        return
+    return x * fact(x - 1)
+
+
+x = int(input())
+print(fact(x))
+
+
+# --------------------------------------------------------------------------------
 print("\n \n  Coding Exercise 6: \n")
 
 
@@ -56,3 +93,37 @@ crowd_test(s)
 s.pop()
 s.pop()
 crowd_test(s)
+
+
+def crowd_test2(s):
+    if len(s) >= 3:
+        print("The room is crowded")
+    else:
+        print("The room is not very crowded")
+
+
+crowd_test2(s)
+
+
+def crowd_test3(s):
+    if len(s) >= 5:
+        print("There is a mob in the room")
+    elif len(s) >= 3:
+        print("The room is crowded")
+    elif len(s) >= 1:
+        print("The room is not very crowded")
+    else:
+        print("The room is empty")
+
+
+s = ["Amber", "Alan", "Mark", "Sammy", "Maggie", "Justin"]
+crowd_test3(s)
+s.pop()
+s.pop()
+crowd_test3(s)
+s.pop()
+s.pop()
+crowd_test3(s)
+s.pop()
+s.pop()
+crowd_test3(s)
